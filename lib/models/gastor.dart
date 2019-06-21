4eaf3098e50dc38a06b589c4c32c997c2470bf94
Ivan Nuñez <ivanluis.cba@gmail.com>
2019-06-21 17:39:41 -0300

    Create gastor model

--- lib/models/gastor.dart
+++ lib/models/gastor.dart
@@ -1,1 +1,16 @@
-class Gastor {}
+class Gastor {
+  var ammount;
+  String currency;
+
+  Gastor({
+    this.ammount,
+    this.currency,
+  });
+
+  factory Gastor.fromJson(json) {
+    return Gastor(
+      ammount: json['ammount'],
+      currency: json['currency'],
+    );
+  }
+}
